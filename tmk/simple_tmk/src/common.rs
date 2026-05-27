ce8eb1e86045273a828629800c9bb4d2f0cd416e
Flgodd67 <b-bearam@microsoft.com>
2026-05-27 11:50:06 +0100

    cca: debugging

--- tmk/simple_tmk/src/common.rs
+++ tmk/simple_tmk/src/common.rs
@@ -5,7 +5,21 @@
 
 use crate::prelude::*;
 
-// #[tmk_test]
-// fn boot(_: TestContext<'_>) {
-//     log!("hello world");
-// }
+#[tmk_test]
+fn boot(_: TestContext<'_>) {
+    log!("hello world");
+}
+
+#[tmk_test]
+fn instruction_abort_outside_par(_: TestContext<'_>) {
+    log!("instruction_abort_outside_par");
+
+    let target = 0x0000_ffff_0000_0000usize;
+
+    unsafe {
+        let f: extern "C" fn() = core::mem::transmute(target);
+        f();
+    }
+
+    panic!("branch to outside PAR unexpectedly returned");
+}
