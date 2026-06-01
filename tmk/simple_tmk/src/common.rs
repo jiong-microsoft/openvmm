c3b62634d07aeb24018463aba71d652eabbab28e
Flgodd67 <b-bearam@microsoft.com>
2026-06-01 10:30:26 +0100

    cca: trying to add a test that triggers an instruction abort

--- tmk/simple_tmk/src/common.rs
+++ tmk/simple_tmk/src/common.rs
@@ -10,20 +10,20 @@ fn boot(_: TestContext<'_>) {
     log!("hello world");
 }
 
-// core::arch::global_asm! {
-//     "instruction_abort_outside_par_trampoline:",
-//     "movz x16, #0x0000",
-//     "movk x16, #0x0000, lsl #16",
-//     "movk x16, #0xffff, lsl #32",
-//     "movk x16, #0x0000, lsl #48",
-//     "br x16",
-// }
+core::arch::global_asm! {
+    "instruction_abort_outside_par_trampoline:",
+    "movz x16, #0x0000",
+    "movk x16, #0x0000, lsl #16",
+    "movk x16, #0xffff, lsl #32",
+    "movk x16, #0x0000, lsl #48",
+    "br x16",
+}
 
-// #[tmk_test]
-// fn instruction_abort_outside_par(_: TestContext<'_>) {
-//     log!("instruction_abort_outside_par");
+#[tmk_test]
+fn instruction_abort_outside_par(_: TestContext<'_>) {
+    log!("instruction_abort_outside_par");
 
-//     instruction_abort_outside_par_trampoline();
+    instruction_abort_outside_par_trampoline();
 
-//     panic!("branch to outside PAR unexpectedly returned");
-// }
+    panic!("branch to outside PAR unexpectedly returned");
+}
